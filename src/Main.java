a0208fb59e26f427a6e0b5e35f75dac66759e32d
Anastasia <N.S.Morsik@yandex.ru>
2025-03-15 00:04:11 +0300

    fix bugs

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+
         File file = new File("data.csv");
         TaskManager taskManager = Managers.getFileBackTaskManager(file);
 
@@ -57,7 +57,6 @@ public class Main {
         }
         System.out.println("-------");
     }
-
 }
 
 
